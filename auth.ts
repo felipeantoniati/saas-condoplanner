abb766bae9fe1942ad632da6a3a99a3ed23272e3
felipeantoniati <ajudaformal@gmail.com>
2024-01-16 14:58:26 -0300

    NextAuth: Implementação de callBacks para atribuir 'id' e 'role' para aos usuários

--- auth.ts
+++ auth.ts
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth"
 
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { UserRole } from "@prisma/client"
 
 import authConfig from "@/auth.config"
+import { db } from "@/lib/db"
+import { getUserById } from "@/utils/fetchData/user"
 
-const prisma = new PrismaClient()
 
 export const {
   handlers: { GET, POST },
@@ -13,7 +14,32 @@ export const {
   signIn,
   signOut
 } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    }
+  },
+  adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
 })
